45ab0fe47f061c8553f2a7f5d841e6e4f4319cd9
Diego Martínez <59999609+diegomarty00@users.noreply.github.com>
2023-11-16 12:59:38 +0100

    Update Ejemplo4.m

--- lab4/Ejemplo4.m
+++ lab4/Ejemplo4.m
@@ -1,1 +1,27 @@
+%======== EJERCICIO 1 ========
+syms a b x
+f1a=1/(1+exp(x));
+f1b=sec(x);
+f1c=exp(a*x)*sin(b*x);
+f1d=x^3*log(x);
+f1e=asin(x);
+f1f=x*atan(sqrt(x^2-1));
+pretty(int(f1f,x))
 
+
+%======== EJERCICIO 2 ========
+
+
+%======== EJERCICIO 3 ========
+
+
+%======== EJERCICIO 4 ========
+
+
+%======== EJERCICIO 5 ========
+
+
+%======== EJERCICIO 6 ========
+
+
+%======== EJERCICIO 7 ========
